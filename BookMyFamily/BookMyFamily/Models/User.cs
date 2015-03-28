5094c2779f22bb6c0b0c8d84b1902803ea38eeb7
Darko Kotoski <wbountyhunter@hotmail.com>
2015-03-28 01:31:20 +0100

    Account Controller
    Implement Some functions

---
+++ BookMyFamily/BookMyFamily/Models/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookMyFamily.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Born { get; set; }
+        public string Location { get; set; }
+        public List<User> Parents { get; set; }
+        public List<User> Children { get; set; }
+
+        public User(string Name, string Surname, DateTime Born, string Location)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Born = Born;
+            this.Location = Location;
+        }
+
+        public void AddParent(User parrent)
+        {
+            try
+            {
+                if (Parents.Count <= 2)
+                {
+                    Parents.Add(parrent);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cant have more then 2 parents");
+            }
+            
+        }
+
+        public void AddChild(User child)
+        {
+            Children.Add(child);
+        }
+    }
+}
